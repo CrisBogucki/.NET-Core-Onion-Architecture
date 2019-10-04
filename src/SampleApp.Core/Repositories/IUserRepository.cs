2ed0e3270b13bde0c9dc9266dcf853daae3bc016
Krzysztof Bogucki <krzysztof.lubien@icloud.com>
2019-10-04 16:29:03 +0200

    add method of user repository

--- src/SampleApp.Core/Repositories/IUserRepository.cs
+++ src/SampleApp.Core/Repositories/IUserRepository.cs
@@ -1,7 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SampleApp.Core.Domain;
+
 namespace SampleApp.Core.Repositories
 {
     public interface IUserRepository
     {
-        
+        Task<User> GetAsync(Guid id); 
+        Task<IEnumerable<User>> BrowseAsync(string firstName = "", string lastName = "");
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);      
     }
 }
